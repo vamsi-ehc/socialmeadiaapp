ae52dfe8e817a9a8add52d99dad8d3729a8aaf0a
vamsi-ehc <konathalavamsi123@gmail.com>
2022-07-04 23:58:15 +0530

    update

---
+++ components/Navbar.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
+import { GoogleLogin, googleLogout  } from '@react-oauth/google';
+
+import Logo from '../utils/tiktik-logo.png';
+const Navbar = () => {
+  return (
+    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+    <Link href='/'>
+      <div className='w-[100px] md:w-[129px] md:h-[30px] h-[38px]'>
+        <Image
+          className='cursor-pointer'
+          src={Logo}
+          alt='logo'
+          layout='responsive'
+        />
+      </div>
+    </Link>
+    </div>
+      
+  )
+}
+
+export default Navbar
---
+++ components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import link from 'next/link';
+import GoogleLogin from 'react-google-login';
+import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
+const Sidebar = () => {
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+export default Sidebar
